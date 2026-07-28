e1ae8c96ae4094f9434614abfb0cd4feca8057cf
yashasvi gupta <yashasvi655@gmail.com>
2026-07-28 22:46:00 +0530

    Fixed API base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://prepai-1-0uy4.onrender.com",
+  baseURL: "https://prepai-1-0uy4.onrender.com/api",
 });
 
-// Automatically attach the token to every request if it exists
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
