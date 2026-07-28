276904fe3c23846b1183b23d6c48e8818ea95e80
yashasvi gupta <yashasvi655@gmail.com>
2026-07-28 20:34:18 +0530

    Updated API URL for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://prepai-1-0uy4.onrender.com",
 });
 
 // Automatically attach the token to every request if it exists
